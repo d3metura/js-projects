73c9ff1992619fdf4f696bd6091781f515d5d933
d3metura <113028037+d3metura@users.noreply.github.com>
2023-01-28 23:38:36 -0800

    Password Generator Completed

--- random-password-generator/app.js
+++ random-password-generator/app.js
@@ -1,93 +1,39 @@
-const characters = [
-  "A",
-  "B",
-  "C",
-  "D",
-  "E",
-  "F",
-  "G",
-  "H",
-  "I",
-  "J",
-  "K",
-  "L",
-  "M",
-  "N",
-  "O",
-  "P",
-  "Q",
-  "R",
-  "S",
-  "T",
-  "U",
-  "V",
-  "W",
-  "X",
-  "Y",
-  "Z",
-  "a",
-  "b",
-  "c",
-  "d",
-  "e",
-  "f",
-  "g",
-  "h",
-  "i",
-  "j",
-  "k",
-  "l",
-  "m",
-  "n",
-  "o",
-  "p",
-  "q",
-  "r",
-  "s",
-  "t",
-  "u",
-  "v",
-  "w",
-  "x",
-  "y",
-  "z",
-  "0",
-  "1",
-  "2",
-  "3",
-  "4",
-  "5",
-  "6",
-  "7",
-  "8",
-  "9",
-  "~",
-  "`",
-  "!",
-  "@",
-  "#",
-  "$",
-  "%",
-  "^",
-  "&",
-  "*",
-  "(",
-  ")",
-  "_",
-  "-",
-  "+",
-  "=",
-  "{",
-  "[",
-  "}",
-  "]",
-  ",",
-  "|",
-  ":",
-  ";",
-  "<",
-  ">",
-  ".",
-  "?",
-  "/",
-];
+const characters=["A","B","C","D","E","F","G",
+"H","I","J","K","L","M","N","O","P","Q","R",
+"S","T","U","V","W","X","Y","Z","a","b","c",
+"d","e","f","g","h","i","j","k","l","m","n",
+"o","p","q","r","s","t","u","v","w","x","y",
+"z", "0", "1", "2", "3", "4", "5", "6", "7",
+ "8", "9","~","`","!","@","#","$","%","^",
+ "&","*","(",")","_","-","+","=","{",
+ "[","}","]",",","|",":",";","<",">",".","?",
+"/"];
+
+const randomPasswordEl1 = document.querySelector(".random-password-el-1")
+const randomPasswordEl2 = document.querySelector(".random-password-el-2")
+const generateButtonEl = document.querySelector(".generate-button-el")
+
+let passwordLength = 10
+
+function generateRandomPassword() {
+    let randomPassword = ""
+    for (let i=1; i <= passwordLength; i++) {
+        let randomNum = Math.floor(Math.random() * characters.length)
+        let randomChar = characters[randomNum]
+        randomPassword += randomChar;
+    }
+    return randomPassword
+}
+
+function generateFirstPassword() {
+    randomPasswordEl1.textContent = generateRandomPassword()
+}
+function generateSecondPassword() {
+    randomPasswordEl2.textContent = generateRandomPassword()
+}
+
+generateButtonEl.addEventListener("click", () => {
+    generateFirstPassword()
+    generateSecondPassword()
+})
+
