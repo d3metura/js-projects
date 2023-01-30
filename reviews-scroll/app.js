d763ad1fccdadb76a14d3769f5ea3e28610d8707
d3metura <113028037+d3metura@users.noreply.github.com>
2023-01-30 02:11:25 -0800

    Finished

--- reviews-scroll/app.js
+++ reviews-scroll/app.js
@@ -4,7 +4,8 @@ const imgEl = document.querySelector("#img-el");
 const nameEl = document.querySelector("#name-el");
 const jobEl = document.querySelector("#job-el");
 const textEl = document.querySelector("#text-el");
-const nextBtn = document.querySelector(".next-btn");
+const forthBtn = document.querySelector(".forth-btn");
+const backBtn = document.querySelector(".back-btn");
 
 let currentIndex = 0;
 imgEl.src = reviews[currentIndex].img;
@@ -12,7 +13,7 @@ nameEl.textContent = reviews[currentIndex].name;
 jobEl.textContent = reviews[currentIndex].job;
 textEl.textContent = reviews[currentIndex].text;
 
-function changeIndex() {
+function nextIndex() {
   currentIndex = (currentIndex + 1) % reviews.length;
   imgEl.src = reviews[currentIndex].img;
   nameEl.textContent = reviews[currentIndex].name;
@@ -20,4 +21,13 @@ function changeIndex() {
   textEl.textContent = reviews[currentIndex].text;
 }
 
-nextBtn.addEventListener("click", changeIndex);
+function prevIndex() {
+  currentIndex = (currentIndex - 1 + reviews.length) % reviews.length;
+  imgEl.src = reviews[currentIndex].img;
+  nameEl.textContent = reviews[currentIndex].name;
+  jobEl.textContent = reviews[currentIndex].job;
+  textEl.textContent = reviews[currentIndex].text;
+}
+
+forthBtn.addEventListener("click", nextIndex);
+backBtn.addEventListener("click", prevIndex);
