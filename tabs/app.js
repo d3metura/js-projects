85a932d5029a9679eb65c22fe4dcdb5de0a978e5
d3metura <113028037+d3metura@users.noreply.github.com>
2023-02-03 03:13:58 -0800

    tabs app working without styling

--- tabs/app.js
+++ tabs/app.js
@@ -1,13 +1,48 @@
 const tabsContainer = document.querySelector(".tabs-container");
-const titles = ["history", "vision", "goals"];
-let selectedTitle = titles[0];
+const infoContainer = document.querySelector(".info-container");
+const tabs = [
+  {
+    id: "his",
+    title: "History",
+    selected: false,
+    description: "this is history",
+  },
+  {
+    id: "ath",
+    title: "Another Thing",
+    selected: false,
+    description: "this is anoter thing",
+  },
+  {
+    id: "sth",
+    title: "Something",
+    selected: false,
+    description: "this is something",
+  },
+];
 
-titles.forEach((title) => {
+let selectedTabIndex = 0;
+
+tabs.forEach((tab, index) => {
   const span = document.createElement("span");
-  span.textContent = title;
+  tab.element = span;
+  span.textContent = tab.title;
   span.addEventListener("click", () => {
-    selectedTitle = title;
-    console.log(selectedTitle);
+    selectedTabIndex = index;
+    updateUI();
   });
   tabsContainer.appendChild(span);
 });
+
+updateUI();
+
+function updateUI() {
+  tabs.forEach((tab, index) => {
+    if (index == selectedTabIndex) {
+      tab.element.style.backgroundColor = "white";
+    } else {
+      tab.element.style.backgroundColor = "#dbdbdb";
+    }
+  });
+  infoContainer.textContent = tabs[selectedTabIndex].description;
+}
